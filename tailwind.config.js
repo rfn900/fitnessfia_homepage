488b442fecb0e3140799ff8d27da0b6d48b1e012
rfn900 <rfn900@gmail.com>
2021-06-11 12:09:29 +0200

    Initial commit. Implement hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,54 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: {
-    content: ['./public/**/*.html'],
+    content: ["./public/**/*.html"],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Raleway", "Sans Serif"],
+      body: ["Open Sans"],
+    },
+    extend: {
+      colors: {
+        rose: colors.rose,
+        violet: colors.violet,
+      },
+      fontSize: {
+        "10xl": "10rem",
+      },
+      letterSpacing: {
+        large: "0.3em",
+      },
+      height: {
+        "2px": "2px",
+        mobile: "748px",
+        laptop: "900px",
+        desktop: "1080px",
+        wide: "1440px",
+      },
+      width: {
+        desktop: "1680px",
+      },
+      maxHeight: {
+        mobile: "748px",
+        laptop: "900px",
+        desktop: "1080px",
+      },
+      maxWidth: {
+        desktop: "1680px",
+      },
+      minHeight: {
+        laptop: "900px",
+      },
+      screens: {
+        "3xl": "2120px",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
